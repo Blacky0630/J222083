7f756cee2a5999d9952d767af89bbfb60812e0de
Blacky0630 <122036891+Blacky0630@users.noreply.github.com>
2025-01-10 21:42:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.gallery-nav a'); // カテゴリボタン
-    const categories = document.querySelectorAll('.gallery-category'); // カテゴリセクション
+    const navLinks = document.querySelectorAll('.gallery-nav a'); 
+    const categories = document.querySelectorAll('.gallery-category'); 
 
     navLinks.forEach(link => {
 		link.addEventListener('click', event => {
-			event.preventDefault(); // デフォルト動作を無効化
+			event.preventDefault(); 
 	
-			// 全カテゴリを非表示
+			// 非表示
 			categories.forEach(category => category.classList.remove('active'));
 	
-			// 対象のカテゴリを表示
+			
 			const targetCategory = document.getElementById(link.dataset.category);
 			if (targetCategory) {
 				targetCategory.classList.add('active');
@@ -18,4 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
     // 初期表示
     document.getElementById('Fanart').classList.add('active');
+});
+const modal = document.getElementById('image-modal');
+const modalImage = document.getElementById('modal-image');
+const closeModal = document.querySelector('.modal .close');
+
+
+const galleryImages = document.querySelectorAll('.gallery-grid img');
+galleryImages.forEach(img => {
+	img.addEventListener('click', () => {
+		modal.style.display = 'block'; 
+		modalImage.src = img.src; 
+	});
+});
+
+
+closeModal.addEventListener('click', () => {
+	modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (event) => {
+	if (event.target === modal) {
+		modal.style.display = 'none';
+	}
 });
